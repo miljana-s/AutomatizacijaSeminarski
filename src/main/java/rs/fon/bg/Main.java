ea76a08c254064e72d393d2716e390455157f1c1
miljanica <miljana.mimy95@gmail.com>
2023-05-02 18:30:01 +0200

    Hotfix add Circle class

--- src/main/java/rs/fon/bg/Main.java
+++ src/main/java/rs/fon/bg/Main.java
@@ -1,7 +1,12 @@
 package rs.fon.bg;
 
+import rs.fon.bg.shapes.Circle;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        Circle c = new Circle(3.5);
+        System.out.println("Circle Area : " + c.getArea() + "\nCircle Perimeter : " + c.getPerimeter());
     }
+
 }
